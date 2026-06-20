943383ffe95afe6df78c59d6127ac5ff2c61c3ae
B33th0v3n960 <zhousun06@gmail.com>
2026-06-20 22:43:31 +0100

    build: changed the localhost port to 3000

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: "0.0.0.0", // Optional: allows external access
+  },
+});
