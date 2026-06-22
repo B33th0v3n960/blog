4d0c66e60120b23912eb09d165ee15ad7d352900
B33th0v3n960 <zhousun06@gmail.com>
2026-06-22 23:06:54 +0100

    chore: format code

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector(".header__menu");
 
 menu?.addEventListener("click", () => {
-    const isExpanded = menu.getAttribute("aria-expanded") === "true";
-    menu.setAttribute("aria-expanded", `${!isExpanded}`);
+  const isExpanded = menu.getAttribute("aria-expanded") === "true";
+  menu.setAttribute("aria-expanded", `${!isExpanded}`);
 });
