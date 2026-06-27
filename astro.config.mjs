b26e5ae1b3c1ff37929f07f44f26cd68c8d3011e
B33th0v3n960 <zhousun06@gmail.com>
2026-06-27 19:20:25 +0100

    feat: added latex rendering support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
-// @ts-check
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { remarkAlert } from "remark-github-blockquote-alert";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
-// https://astro.build/config
 export default defineConfig({
+  integrations: [mdx(), sitemap()],
   server: {
     port: 3000,
     host: "0.0.0.0", // Optional: allows external access
   },
   markdown: {
-    remarkPlugins: [remarkAlert],
+    remarkPlugins: [remarkAlert, remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
         light: "gruvbox-light-hard",
--- src/data/blog/post-3.md
+++ src/data/blog/post-3.md
@@ -132,6 +132,7 @@ $$
 \int_0^\infty e^{-x^2} dx = \frac{\sqrt{\pi}}{2}
 $$
 ```
+inline: $E = mc^2$
 
 $$
 \int_0^\infty e^{-x^2} dx = \frac{\sqrt{\pi}}{2}
